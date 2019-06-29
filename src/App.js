77dbc0d3d39c3e7b8c797f9b2e9a910a13aa61f2
Friedrich TANE <bftane@gmail.com>
2019-06-29 23:49:17 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Connexion from './components/Connexion';
 
-export default App;
+
+export default class App extends Component {
+
+  render(){
+
+    return (
+      <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
+        <Switch>
+          <Route exact path='/' component={Connexion} />
+          {/* <Route exact path='/index.html' render={() => <Home stores={this.state.items}/>} /> */}
+          {/* <Route component={ModalSwitch}/> */}
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+};
---
+++ src/components/Connexion.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Connexion extends Component {
+
+    render(){
+
+        return(
+            <div>Connectez vous un peu</div>
+        );
+    }
+}
